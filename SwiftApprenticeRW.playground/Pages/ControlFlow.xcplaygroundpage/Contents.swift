4b6c3823e798fa9fd1be1459537700a2976f9c6d
syntakks <syntakks@gmail.com>
2021-08-23 10:27:25 -0500

    Adds Doxygen Page

--- SwiftApprenticeRW.playground/Pages/ControlFlow.xcplaygroundpage/Contents.swift
+++ SwiftApprenticeRW.playground/Pages/ControlFlow.xcplaygroundpage/Contents.swift
@@ -7,6 +7,7 @@ let closedRange = 0...5
 // Does not include the 5
 let halfOpenRange = 0..<5
 
+//MARK: - For Loop
 // Conditional For loop
 let count = 10
 var sum = 0
@@ -14,6 +15,46 @@ for i in 1...count where i % 2 == 1 {
   sum += 1
 }
 sum
+
+//MARK: - Advanced Switch Statements
+let hourOfDay = 12
+var timeOfDay = ""
+
+// Comma separation
+switch hourOfDay {
+case 0, 1, 2, 3, 4, 5:
+  timeOfDay = "Early morning"
+case 6, 7, 8, 9, 10, 11:
+  timeOfDay = "Morning"
+case 12, 13, 14, 15, 16:
+  timeOfDay = "Afternoon"
+case 17, 18, 19:
+  timeOfDay = "Evening"
+case 20, 21, 22, 23:
+  timeOfDay = "Late evening"
+default:
+  timeOfDay = "INVALID HOUR!"
+}
+
+timeOfDay
+
+// Ranges
+switch hourOfDay {
+case 0...5:
+  timeOfDay = "Early morning"
+case 6...11:
+  timeOfDay = "Morning"
+case 12...16:
+  timeOfDay = "Afternoon"
+case 17...19:
+  timeOfDay = "Evening"
+case 20..<24:
+  timeOfDay = "Late evening"
+default:
+  timeOfDay = "INVALID HOUR!"
+}
+
+timeOfDay
 
 
 //: [Next](@next)
---
+++ SwiftApprenticeRW.playground/Pages/Doxygen.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+//: [Previous](@previous)
+
+import UIKit
+
+/// This func will return the number of characters in a given string.
+/// - Parameter word: This is the string that will be measured.
+/// - Returns: Int value of the number of characters in the word.
+func wordLength(word: String) -> Int {
+  return word.count
+}
+
+wordLength(word: "Bobby")
+
+// opt+cmd+/
+// After you write the function, use this shortcut to have xcode auto generate your doxygen comment.
+// This is very helpful for complex functions or frequently used functions in your code.
+
+//: [Next](@next)
