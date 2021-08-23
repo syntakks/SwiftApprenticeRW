552b68d5d9999854b156bb4db7c689fc99d25987
syntakks <syntakks@gmail.com>
2021-08-23 10:12:05 -0500

    Adds conditional for loop

--- SwiftApprenticeRW.playground/Pages/ControlFlow.xcplaygroundpage/Contents.swift
+++ SwiftApprenticeRW.playground/Pages/ControlFlow.xcplaygroundpage/Contents.swift
@@ -7,6 +7,13 @@ let closedRange = 0...5
 // Does not include the 5
 let halfOpenRange = 0..<5
 
+// Conditional For loop
+let count = 10
+var sum = 0
+for i in 1...count where i % 2 == 1 {
+  sum += 1
+}
+sum
 
 
 //: [Next](@next)
